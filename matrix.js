d70ac212f0f759458f1641629a7bea1d78fc49b4
denisdbz <dns.gtv@gmail.com>
2025-04-18 18:41:11 -0300

    update index.html, style.css e matrix.js

--- matrix.js
+++ matrix.js
@@ -1,23 +1,32 @@
-
-const canvas = document.getElementById('matrix-canvas');
+const canvas = document.getElementById('matrix');
 const ctx = canvas.getContext('2d');
+
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
-const letters = "01";
+
+const letters = '01';
 const fontSize = 14;
 const columns = canvas.width / fontSize;
+
 const drops = Array(Math.floor(columns)).fill(1);
+
 function draw() {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "#0F0";
-  ctx.font = fontSize + "px monospace";
-  drops.forEach((y, i) => {
+
+  ctx.fillStyle = '#0F0';
+  ctx.font = fontSize + 'px monospace';
+
+  for (let i = 0; i < drops.length; i++) {
     const text = letters[Math.floor(Math.random() * letters.length)];
-    const x = i * fontSize;
-    ctx.fillText(text, x, y * fontSize);
-    if (y * fontSize > canvas.height && Math.random() > 0.975) drops[i] = 0;
+    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+
+    if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+      drops[i] = 0;
+    }
+
     drops[i]++;
-  });
+  }
 }
+
 setInterval(draw, 33);
