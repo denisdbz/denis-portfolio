ba781c3377a5c8cd662130f908116306363162b3
denisdbz <dns.gtv@gmail.com>
2025-04-20 19:32:05 -0300

    correção botões executar teste

--- plays/play-01-nmap-recon/run.sh
+++ plays/play-01-nmap-recon/run.sh
@@ -1,3 +1,4 @@
+export PATH=$PATH:/usr/bin/
 #!/bin/bash
 
 TARGET="127.0.0.1"
--- plays/play-03-sqlmap-dvwa/run.sh
+++ plays/play-03-sqlmap-dvwa/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo "Executando SQLMap..."
+
+# Comando SQLMap real para testar vulnerabilidades
+sqlmap -u "http://target.com/vulnerable?id=1" --batch --risk=3 --level=5 -o -t 10 --output-dir=./ --format=HTML
+
+# Gerando relatório em HTML
 cat <<EOF > relatorio.html
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -11,7 +16,7 @@ cat <<EOF > relatorio.html
 <body>
   <h2>Resultado SQLMap</h2>
   <pre>
-$(echo "[*] Simulação do SQLMap: Vulnerabilidade detectada em 'id'")
+$(cat ./output-dir/sqlmap-report.html)
   </pre>
 </body>
 </html>
