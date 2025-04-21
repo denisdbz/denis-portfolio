2a40516e4313604b11dd869b67b0a41feb85a29e
denisdbz <dns.gtv@gmail.com>
2025-04-21 14:55:11 -0300

    chore: adiciona configuração do mochawesome no Cypress

--- plays/play-04-cypress/cypress.config.js
+++ plays/play-04-cypress/cypress.config.js
@@ -1,1 +1,14 @@
-module.exports = { e2e: { baseUrl: 'http://localhost:3000' } };
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports/html',
+      overwrite: false,
+      html: false,
+      json: true
+    }
+  }
+});
