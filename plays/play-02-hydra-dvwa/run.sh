723a4f3000fcf257265d24aecfaac416d74c3006
denisdbz <dns.gtv@gmail.com>
2025-04-28 22:20:04 -0300

    Atualiza play 3, 4, 5 e run do play2

--- plays/play-02-hydra-dvwa/run.sh
+++ plays/play-02-hydra-dvwa/run.sh
@@ -3,5 +3,5 @@
 set -e
 echo "[INFO] Play 02 — Hydra DVWA"
 TARGET="dvwa.local"
-hydra -L users.txt -P pass.txt ${TARGET} http-get /login.php || true
+hydra -L users.txt -P passwords.txt ${TARGET} http-get /login.php || true
 echo "[SUCESSO] Ataque concluído"
