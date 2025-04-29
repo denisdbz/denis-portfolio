3dcdcd051faa3ce3521ffd50ced1f6f423ec8b44
denisdbz <dns.gtv@gmail.com>
2025-04-29 02:06:39 -0300

    UPDATE - novo

---
+++ particles.js
@@ -0,0 +1,26 @@
+/* Particles.js config */
+particlesJS("particles-js", {
+  "particles": {
+    "number": { "value": 80 },
+    "color": { "value": "#00ff9f" },
+    "shape": { "type": "circle" },
+    "opacity": { "value": 0.5 },
+    "size": { "value": 3 },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#00ff9f",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": { "enable": true, "mode": "repulse" }
+    }
+  }
+});
