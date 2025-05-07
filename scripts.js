7c6ec2939a5ea8ebbefb34376d8446ffb88ac734
denisdbz <dns.gtv@gmail.com>
2025-05-07 01:16:04 -0300

    corrige scripts.js

--- scripts.js
+++ scripts.js
@@ -7,7 +7,13 @@ function iniciarStream() {
   outputBox.textContent = 'Iniciando execução...\n';
   progressBar.style.width = '10%';
 
-  const evt = new EventSource('https://135d-177-86-39-143.ngrok-free.app/stream/play-01-nmap-recon');
+  // Obtém o nome do play pela URL do navegador
+  const playPath = window.location.pathname.split('/').filter(Boolean).pop();
+  const playName = playPath.replace('index.html', '').replace(/\/$/, '');
+
+  // Substitua aqui pela URL fixa do seu ngrok atual
+  const baseURL = 'https://135d-177-86-39-143.ngrok-free.app';
+  const evt = new EventSource(`${baseURL}/stream/${playName}`);
 
   evt.onmessage = function (event) {
     outputBox.textContent += event.data + '\n';
@@ -15,9 +21,7 @@ function iniciarStream() {
 
     if (event.data.includes('✔ Teste finalizado')) {
       progressBar.style.width = '100%';
-      setTimeout(() => {
-        evt.close();
-      }, 2000);
+      setTimeout(() => evt.close(), 2000);
     } else {
       progressBar.style.width = Math.min(progressBar.clientWidth + 10, 95) + '%';
     }
